b8b1df6c3f3616a4c6384cb7dea6c2ed09daf031
Songji <32353880+Songjis@users.noreply.github.com>
2018-02-02 09:40:18 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ var gApp = angular.module('gApp', []);
 			 url : "team/team.pdf", 
 			 title: "Team",
 			 name: "Impression",
-			 img: "team/TeamImpression.png",
+			 img: "team/TeamImpression.PNG",
 			 type : true, 
 			 contents: "이번에 팀프로젝트를하면서 저는 백엔드와 데이터베이스를 맡게 되었습니다. 팀프로젝트로 공공기관을 벤치마킹해서 만들면서 평소에 교육을 받으면서 익혔던 데이터베이스 구문들이 많은 도움이 되었고 홈페이지에 특히 기능을 추가하는 작업을 하면서 어려움을 겪을 때가 종종 있었지만 동료들의 도움으로 끝까지 마칠 수 있었던 것 같습니다. 마지막으로 팀프로젝트를 하면서 저의 백엔드와 데이터베이스의 실력을 더 쌓을수 있었고 끝까지 열심히 팀프로젝트를 한 팀원들에게 고마운 마음이 듭니다. "
 			},{
@@ -57,7 +57,7 @@ var gApp = angular.module('gApp', []);
 			 url : "personal/personal.pdf", 
 			 title: "Personal",
 			 name: "Impression",
-			 img: "personal/PersonalImpression.png",
+			 img: "personal/PersonalImpression.PNG",
 			 type : false,
 			 contents: " 저는 교육받은 내용으로 개인프로젝트를 만들면서 모든 구상과 결정을 혼자서 해야한다는 막막함이 초반에 있었습니다. 저는 전부터 쇼핑몰 웹 사이트를 만들고 싶었고 쇼핑몰은 기능이 많아서 웹사이트의 주제를 바꿀까도 고민을 많이 했지만 그래도 개인프로젝트인데 하고싶은 것을 내가 할 수 있는 한 다하자라는 마음을 갖고 만들었습니다. 프로젝트를 만들면서 한달 정도 걸렸는데 만드는 과정에서 막히는 부분이 많았고 그때마다 주변 사람들에게 물어보고 저 혼자서 찾아보면서 프로젝트를 해나갔습니다. 그리고 프로젝트를 만들면서 저번에 팀플 때 했던 코드들이 도움이 되었습니다. 또, 개인프로젝트를 하면서 부분적으로 새로운 방법을 배워서 구현을 해봐서 좋았던 것 같고, 시간을 맞춰야해서 기능을 뺀 부분들이 좀 있고 추가로 예외처리해야할 부분들도 있어서 아쉽지만 부족한 부분은 나중에 보충해서 웹사이트를 꼭 완성시키고 싶다는 생각이 들었습니다. "
 			},{
@@ -65,7 +65,7 @@ var gApp = angular.module('gApp', []);
 			 url : "personal.mp4", 
 			 title: "Personal",
 			 name: "Media",
-			 img: "personal/PersonalMedia.png",
+			 img: "personal/PersonalMedia.PNG",
 			 type : false, 
 			 contents: ""
 			}
